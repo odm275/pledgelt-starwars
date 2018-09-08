6ad121458afbfbee9b16b95d3c81dacaf5a2f3b2
odm275 <pomejia@gmail.com>
2018-09-08 13:44:47 -0500

    UI/UX card mostly finished

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Character from "./Character";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: []
+    };
+    this.getCharacters = this.getCharacters.bind(this);
+  }
+  componentDidMount() {
+    this.getCharacters();
+  }
+  async getCharacters() {
+    const { data } = await axios(`https://swapi.co/api/people`);
+    this.setState({ characters: data.results });
+  }
+
+  onChange() {}
+
+  render() {
+    const { characters } = this.state;
+    return (
+      <div className="App">
+        <h1 className="welcome">Select a Film</h1>
+        <input />
+        <Character />
+      </div>
+    );
+  }
+}
+
+export default App;
